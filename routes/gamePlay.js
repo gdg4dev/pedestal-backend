8ea181455710c79c053ac1215b0e035551712a1e
Aniket Hirpara <gdg4dev@gmail.com>
2023-09-23 23:32:43 -0400

    fixed GamePlay

--- routes/gamePlay.js
+++ routes/gamePlay.js
@@ -21,42 +21,27 @@ router.put("/join", async (req, res, next) => {
 	const { primaryAddr, secondaryAddr, gameId } = req.body;
 	// check if player exists
 
+try{
     if (await playerExists(primaryAddr)) {
-        if (await gameExists(gameExists)) {
-            await contests.findOneAndUpdate({_id: gameId},  { $push: { players: secondaryAddr } },   { new: true }, async (err, updatedGame) => {
-                if (err) {
-                    console.error('Error adding player to game:', err);
-                    return res.status(500).send({
-                        message: "Error adding player to the game",
-                        logs: err
-                    })
-                }
-                if (updatedGame) {
-                    let kvPair ={gameId, secondaryAddr}
-                    await players.findOneAndUpdate({primary: primaryAddr},  { $push: { players: kvPair } } ,  { new: true }, async (err, updatedPlayer) => {
-                        if (err) {
-                            console.error('Error adding game history to player:', err);
-                            return res.status(500).send({
-                                message: "Error adding game history to player",
-                                logs: err
-                            })
-                        }
-
-                        if (updatedPlayer) {
-                            res.status(200).send({message: "success", data: updatedGame})
-                        }
-
-                    })
-                }
-            })
+        if (await gameExists(gameId)) {
+            let updatedGame = await contests.findOneAndUpdate({_id: gameId},  { $push: { players: secondaryAddr } },   { new: true })
+            if (!updatedGame) return  res.status(404).send({message: "game not found"})
+            let kvPair ={gameId, secondaryAddr}
+            let updatedPlayer =  await players.findOneAndUpdate({primary: primaryAddr},  { $push: { players: kvPair } } ,  { new: true })
+            if (!updatedPlayer) return res.status(404).send({message: "player not found"})
+            return res.status(200).send({message: "success", data: updatedGame})
         }
         else {
-            res.status(404).send({message: "game not found"})
+           return  res.status(404).send({message: "game not found"})
         }
     } else {
-        res.status(404).send({message: "player not found"})
+       return res.status(404).send({message: "player not found"})
     }
 
+} catch (e) {
+    console.log(e)
+    return res.status(500).send({message: "server error", log: e})
+}
 	// check if game exists
 
 	// find game -> add player's disposable address to the players array -> go to player's profile and add to secondary
