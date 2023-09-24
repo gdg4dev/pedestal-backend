34d1c6f77fb0f39a6102f1d0a2e1fbfdf2c35bde
Aniket Hirpara <gdg4dev@gmail.com>
2023-09-24 02:11:15 -0400

    fixed put deposits and removed get deposits

--- routes/gamePlay.js
+++ routes/gamePlay.js
@@ -55,46 +55,32 @@ router
 			return res.status(500).send({ message: "server error", log: e });
 		}
 	})
-
-	.get("/deposits", async (req, res, next) => {
-        const { primaryAddr, secondaryAddr, gameId } = req.body;
-       try {
-        if (await playerExists(primaryAddr)) {
-            if (await gameExists(gameId)) {
-                let updatedGame = await contests.findOneAndUpdate(
-                    { _id: gameId },
-                    { $push: { desposits: secondaryAddr } },
-                    { new: true }
-                );
-                if (updatedGame) {
-                    return res.status(200).json({
-                        message: "added the player to deposit list",
-                        data: updatedGame
-                    })
-                }
-            } else {
-                return res.status(404).send({ message: "game not found" });
-            }
-        } else {
-            return res.status(404).send({ message: "player not found" });
-        }
-       } catch (e) {
-             console.log(e);
-			return res.status(500).send({ message: "server error", log: e });
-       }
-		// const {gameId}
-	})
 	.put("/deposits", async (req, res, next) => {
         const { primaryAddr, secondaryAddr, gameId } = req.body;
-        if (await playerExists(primaryAddr)) {
-            if (await gameExists(gameId)) {
-                
+        try {
+            if (await playerExists(primaryAddr)) {
+                if (await gameExists(gameId)) {
+                    let updatedGame = await contests.findOneAndUpdate(
+                        { _id: gameId },
+                        { $push: { desposits: secondaryAddr } },
+                        { new: true }
+                    );
+                    if (updatedGame) {
+                        return res.status(200).json({
+                            message: "added the player to deposit list",
+                            data: updatedGame
+                        })
+                    }
+                } else {
+                    return res.status(404).send({ message: "game not found" });
+                }
             } else {
-                return res.status(404).send({ message: "game not found" });
+                return res.status(404).send({ message: "player not found" });
             }
-        } else {
-            return res.status(404).send({ message: "player not found" });
-        }
+           } catch (e) {
+                 console.log(e);
+                return res.status(500).send({ message: "server error", log: e });
+           }
     });
 
 module.exports = router;
