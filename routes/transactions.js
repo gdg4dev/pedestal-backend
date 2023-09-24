7c242753363677df8cda32961bc793cdd88b6757
Aniket Hirpara <gdg4dev@gmail.com>
2023-09-24 06:43:30 -0400

    added /transactions/one endpoint

--- routes/transactions.js
+++ routes/transactions.js
@@ -15,6 +15,18 @@ router
         res.status(500).send({message: "something went wrong with tx", logs: error})
     }
 })
+.get("/one/", async (req, res, next) => {
+    let {secondaryAddr} = req.body
+    try {
+        let transactions1 = await transactions.findOne({secondary: secondaryAddr})
+        if (!transactions1) res.status(400).json({message: 'no transactions found globally'})
+        console.log(transactions1)
+        res.status(200).send({message: "found", data: transactions1})
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({message: "something went wrong with tx", logs: error})
+    }
+})
 
 
 module.exports = router;
